3e8cf8ccbe42363cc289c2fb66265f6b0c8c0a3c
Steven Dahdah <sdahdah@users.noreply.github.com>
2020-07-29 20:55:34 -0400

    Add skeleton package with entry point

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='pdflookup',
+    version='0.1.0',
+    description='Command line tool to find BibTeX for academic papers using '
+                'Crossref',
+    long_description=readme,
+    author='Steven Dahdah',
+    url='https://github.com/sdahdah/pdflookup',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['pdflookup=pdflookup:main'],
+    }
+)
