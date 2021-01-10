b6a0faedf1a3816eb47a6b86d56409d9f6c67c89
Steven Dahdah <sdahdah@users.noreply.github.com>
2021-01-10 18:41:19 -0500

    Repeat queries by page

--- pdflookup/pdflookup.py
+++ pdflookup/pdflookup.py
@@ -2,6 +2,9 @@ import argparse
 import pdftotext
 import re
 import habanero
+import pathlib
+import sys
+import logging
 
 
 def main():
@@ -12,7 +15,15 @@ def main():
                             help='path to PDF file.')
     args = arg_parser.parse_args()
 
-    # TODO validate input
+    # Validate input
+    file_path = pathlib.Path(args.file)
+    if not file_path.exists():
+        logging.critical(f'Specified config file `{args.file}` does not '
+                         'exist.')
+        sys.exit(1)
+    if not file_path.is_file():
+        logging.critical(f'Specified config file `{args.file}` is not a file.')
+        sys.exit(1)
 
     with open(args.file, 'rb') as f:
         pdf = pdftotext.PDF(f)
@@ -20,20 +31,33 @@ def main():
     # TODO Decide how much text to query with
     # TODO Count number of pages and use more for books
 
-    first_lines = ' '.join(pdf[0].split('\n'))
-    query_raw = re.sub(r'\s+', ' ', first_lines).strip().split(' ')
-    query_unique = []
-    for q in query_raw:
-        if q not in query_unique:
-            query_unique.append(q)
-    query = ' '.join(query_unique[:100]).encode('ascii', 'ignore')
+    query = b''
+    page = 0
+    page_max = 10
 
-    print(' QUERY '.center(80, '='))
-    print(query)
-    print()
+    while query == b'':
+
+        first_lines = ' '.join(pdf[page].split('\n'))
+        query_raw = re.sub(r'\s+', ' ', first_lines).strip().split(' ')
+        query_unique = []
+        for q in query_raw:
+            if q not in query_unique:
+                query_unique.append(q)
+        query = ' '.join(query_unique[:100]).encode('ascii', 'ignore')
+
+        print(f' QUERY {page} '.center(80, '='))
+        print(query)
+        print()
+
+        page += 1
+        if page >= page_max:
+            break
 
     # TODO Decide how many results to limit
     # TODO Decide what fields to return
+
+    print('gonna query now...')
+    breakpoint()
 
     cr = habanero.Crossref()
     result = cr.works(query_bibliographic=query, limit=20)
@@ -44,3 +68,5 @@ def main():
         author = result['message']['items'][i].get('author', ['NO AUTHOR'])[0]
         print(f'({i}) {title}')
         print(f'    {author}')
+
+    breakpoint()
